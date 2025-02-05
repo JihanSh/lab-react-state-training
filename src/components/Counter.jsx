fec21287dfd799f985714d7557ecd155e5542fc9
Jihan Shamas <jihan.shamas@gmail.com>
2025-02-05 15:11:21 +0100

    done

---
+++ src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  };
+  const decreaseCounter = () => {
+    if (counter > 0) setCounter(counter - 1);
+  };
+  return (
+    <div>
+      <button onClick={() => increaseCounter()}> +</button>
+      <p>{counter}</p>
+      <button onClick={() => decreaseCounter()}> -</button>
+    </div>
+  );
+}
+
+export default Counter;
